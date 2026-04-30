1a1f6faa02d59ae63ad13785e26e313ceaeafbd5
andrewjn18 <anuha@sandiego.edu>
2026-04-29 20:19:23 -0700

    feat & fix: update method implementation to pass all current test cases (empty string, single character string, string, and string w/ spaces)

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -2,6 +2,12 @@ package edu.sandiego.comp305;
 
 public class StringUtils {
     public static String reverseString(String strToReverse) {
-        return strToReverse;
+        String reversedStr = "";
+
+        for (int i = strToReverse.length() - 1; i >= 0; i--) {
+            reversedStr += strToReverse.charAt(i);
+        }
+
+        return reversedStr;
     }
 }
