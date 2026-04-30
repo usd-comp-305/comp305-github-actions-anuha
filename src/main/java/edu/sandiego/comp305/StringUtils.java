b6295969b99fe7e6684542c1c42e6e76f7fe06e2
andrewjn18 <anuha@sandiego.edu>
2026-04-29 21:16:58 -0700

    fix: fixed an error concerning a deafault/public constructor and an error to make reversedStr declared as final

--- src/main/java/edu/sandiego/comp305/StringUtils.java
+++ src/main/java/edu/sandiego/comp305/StringUtils.java
@@ -1,8 +1,12 @@
 package edu.sandiego.comp305;
 
-public class StringUtils {
+public final class StringUtils {
+    private StringUtils() {
+
+    }
+
     public static String reverseString(final String strToReverse) {
-        StringBuilder reversedStr = new StringBuilder();
+        final StringBuilder reversedStr = new StringBuilder();
 
         for (int i = strToReverse.length() - 1; i >= 0; i--) {
             reversedStr.append(strToReverse.charAt(i));
